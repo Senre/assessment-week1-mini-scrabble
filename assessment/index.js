ec0274cc979e35d4911fab7df63d273eceab62a5
Senre <kishorshris@gmail.com>
2021-12-09 15:59:23 +0000

    fixed generateLetterTiles

--- assessment/index.js
+++ assessment/index.js
@@ -1,9 +1,9 @@
 // This is an array containing all of your current letters
 // You will be using this array for the tasks below
-export let YOUR_LETTERS = [];
+export let YOUR_LETTERS = []
 
-export function generateLetterTiles(quantity) {
-  const bag = "aaaabbccddeeeeeeffghhiiiijkllmmnnnooopppqrrrssstttuuvwxyyz";
+export function generateLetterTiles(quantity = 8) {
+  const bag = 'aaaabbccddeeeeeeffghhiiiijkllmmnnnooopppqrrrssstttuuvwxyyz'
   /**
    * @todo ================TASK================
    * The 'bag' variable contains a string of random letters.
@@ -12,6 +12,13 @@ export function generateLetterTiles(quantity) {
    * If no number is passed, choose 8 letters
    * Push the random letters to the YOUR_LETTERS array
    * */
+
+  for (let i = 0; i < quantity; i++) {
+    let separatedBag = bag.split('')
+    const pos = Math.floor(Math.random() * separatedBag.length)
+    YOUR_LETTERS.push(separatedBag[pos])
+    separatedBag = separatedBag.splice(pos, 1)
+  }
 }
 
 export function getWordFromPlayer() {
@@ -59,47 +66,47 @@ export function isWordInDictionary(attemptedWord) {
 
 ############################################################################# */
 
-const TOTAL_ROUNDS = 8;
-const HAND_SIZE = 8;
-import data from "./data.js";
-export const dictionary = data.dictionary;
-let points = 0; // Your current points
-let round = 1; // Your current round
+const TOTAL_ROUNDS = 8
+const HAND_SIZE = 8
+import data from './data.js'
+export const dictionary = data.dictionary
+let points = 0 // Your current points
+let round = 1 // Your current round
 
 // For each round:
 function startRound() {
-  console.log(`Round ${round}/${TOTAL_ROUNDS}. "${YOUR_LETTERS.join(" ")}"`);
+  console.log(`Round ${round}/${TOTAL_ROUNDS}. "${YOUR_LETTERS.join(' ')}"`)
 
   // Ask the player for a word
-  const yourWord = getWordFromPlayer();
-  if (!yourWord) return console.log("No word provided");
+  const yourWord = getWordFromPlayer()
+  if (!yourWord) return console.log('No word provided')
 
   // Check they have the correct letters
   if (!isValidGuess(yourWord)) {
-    console.log("You cannot make that word");
-    return startRound();
+    console.log('You cannot make that word')
+    return startRound()
   }
 
   // Check it's in the dictionary
   if (!isWordInDictionary(yourWord)) {
-    console.log("Not found in dictionary");
-    return startRound();
+    console.log('Not found in dictionary')
+    return startRound()
 
     // If so, give them some points and new letters
   } else {
-    console.log(`+${yourWord.length} points! Getting new letters:`);
-    removePlayedLetters(yourWord);
-    generateLetterTiles(yourWord.length);
-    points += yourWord.length;
+    console.log(`+${yourWord.length} points! Getting new letters:`)
+    removePlayedLetters(yourWord)
+    generateLetterTiles(yourWord.length)
+    points += yourWord.length
 
     // And then either move onto the next round
     if (round < TOTAL_ROUNDS) {
-      round++;
-      return startRound();
+      round++
+      return startRound()
 
       // or end the game after the final round
     } else {
-      console.log(`End! Your final score is ${points}`);
+      console.log(`End! Your final score is ${points}`)
     }
   }
 }
@@ -108,11 +115,11 @@ function startRound() {
 function play() {
   console.log(
     `🧩 MiniScrabble: Get as many points as you can in ${TOTAL_ROUNDS} rounds`
-  );
-  const confirmPlay = window.prompt("Play now (y/n)");
-  if (["y", "yes"].includes(confirmPlay.toLowerCase())) {
-    generateLetterTiles(HAND_SIZE);
-    startRound();
+  )
+  const confirmPlay = window.prompt('Play now (y/n)')
+  if (['y', 'yes'].includes(confirmPlay.toLowerCase())) {
+    generateLetterTiles(HAND_SIZE)
+    startRound()
   }
 }
-if (import.meta.main) play();
+if (import.meta.main) play()
